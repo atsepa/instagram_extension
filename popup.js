72c04feedfe266271af71a8c70c5c073b3a7d8f6
Francisco Garcia <beickon@franciscos-mbp.lan>
2020-03-21 08:54:02 -0400

    now using profile pic from memory

--- popup.js
+++ popup.js
@@ -1,115 +1,118 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const bg = chrome.extension.getBackgroundPage();
+document.addEventListener(
+	'DOMContentLoaded',
+	function() {
+		const bg = chrome.extension.getBackgroundPage();
 
-    init();
+		init();
 
-    document.getElementById('settings').addEventListener('click', onclickSettings, false);
+		document.getElementById('settings').addEventListener('click', onclickSettings, false);
 
-    function init() {
-        chrome.storage.sync.get(['insta_username'], function (result) {
-            if(!result.insta_username){
-                console.log('iiiif');
-                // means there is no username set
-                requestUser();
-            } 
-            else if((result.insta_username && !bg) || (result.insta_username && !bg.username) || (result.insta_username && bg.username === ''))
-            {
-                console.log('else iiif user', result.insta_username);
-                console.log('else iiif bg', bg);
-                // means bg did not load
-                // requestUser();
-                tempo();
-                //  maybe do a loading
-                // maybe use localhost data
-                callFetchWith(result.insta_username);
-            } 
-            else {
-                console.log('else');
-                setPopup();
-                callFetch();
-            }
+		function init() {
+			chrome.storage.sync.get([ 'insta_username' ], function(result) {
+				if (!result.insta_username) {
+					console.log('iiiif');
+					// means there is no username set
+					requestUser();
+				} else if (
+					(result.insta_username && !bg) ||
+					(result.insta_username && !bg.username) ||
+					(result.insta_username && bg.username === '')
+				) {
+					// means bg did not load
+					// requestUser();
+					tempo();
+					//  maybe do a loading
+					// maybe use localhost data
+					callFetchWith(result.insta_username);
+				} else {
+					console.log('else');
+					setPopup();
+					callFetch();
+				}
 
-            clearBadge();
-        });
-    }
+				clearBadge();
+			});
+		}
 
-    function tempo() {
-        document.getElementById('popup').style.display = 'grid';
-        document.getElementById('new-user').style.display = 'none';
+		function tempo() {
+			document.getElementById('popup').style.display = 'grid';
+			document.getElementById('new-user').style.display = 'none';
 
-        setCountFromMemory();
-        setUsernameFromMemory();
-        setProfilePicFromMemory();
-    }
+			setCountFromMemory();
+			setUsernameFromMemory();
+			setProfilePicFromMemory();
+		}
 
-    function callFetch() {
-        chrome.runtime.sendMessage({
-            type: 'fetchData'
-        });
-    }
+		function callFetch() {
+			chrome.runtime.sendMessage({
+				type: 'fetchData'
+			});
+		}
 
-    function callFetchWith(insta_username) {
-        chrome.runtime.sendMessage({
-            type: 'fetchDataWith',
-            username: insta_username
-        });
-    }
+		function callFetchWith(insta_username) {
+			chrome.runtime.sendMessage({
+				type: 'fetchDataWith',
+				username: insta_username
+			});
+		}
 
-    function clearBadge(){
-        chrome.runtime.sendMessage({
-            type: 'clearBadge'
-        });
-    }
+		function clearBadge() {
+			chrome.runtime.sendMessage({
+				type: 'clearBadge'
+			});
+		}
 
-    function setPopup() {
-        document.getElementById('popup').style.display = 'grid';
-        document.getElementById('new-user').style.display = 'none';
+		function setPopup() {
+			document.getElementById('popup').style.display = 'grid';
+			document.getElementById('new-user').style.display = 'none';
 
-        setCount();
-        setUsername();
-        setProfilePic();
-    }
+			setCount();
+			setUsername();
+			setProfilePic();
+		}
 
-    function requestUser() {
-        document.getElementById('popup').style.display = 'none';
-        document.getElementById('new-user').style.display = 'block';
-    }
+		function requestUser() {
+			document.getElementById('popup').style.display = 'none';
+			document.getElementById('new-user').style.display = 'block';
+		}
 
-    function onclickSettings() {
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-            chrome.tabs.create({
-                url: './src/settings/settings.html'
-            });
-        });
-    }
-    
-    function setCount () {
-        document.getElementById('followers_count').innerHTML = `${bg.followersCount} followers`;
-    }
+		function onclickSettings() {
+			chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+				chrome.tabs.create({
+					url: './src/settings/settings.html'
+				});
+			});
+		}
 
-    function setCountFromMemory () {
-        chrome.storage.sync.get(['insta_followers_count'], function (result) {
-            document.getElementById('followers_count').innerHTML = `${result.insta_followers_count} followers`;
-        });
-    }
+		function setCount() {
+			document.getElementById('followers_count').innerHTML = `${bg.followersCount} followers`;
+		}
 
-    function setUsername () {
-        document.getElementById('username').innerHTML = `<strong>${bg.username}<strong/>`;
-    }
+		function setCountFromMemory() {
+			chrome.storage.sync.get([ 'insta_followers_count' ], function(result) {
+				document.getElementById('followers_count').innerHTML = `${result.insta_followers_count} followers`;
+			});
+		}
 
-    function setUsernameFromMemory () {
-        chrome.storage.sync.get(['insta_username'], function (result) {
-            document.getElementById('username').innerHTML = `<strong>${result.insta_username}<strong/>`;
-        });
-    }
+		function setUsername() {
+			document.getElementById('username').innerHTML = `<strong>${bg.username}<strong/>`;
+		}
 
-    function setProfilePicFromMemory () {
-        chrome.storage.sync.get(['insta_profile_pic'], function (result) {
-            document.getElementById('profile_pic').innerHTML = `<img src="${bg.profilePic}" />`;
-        });
-    }
+		function setUsernameFromMemory() {
+			chrome.storage.sync.get([ 'insta_username' ], function(result) {
+				document.getElementById('username').innerHTML = `<strong>${result.insta_username}<strong/>`;
+			});
+		}
 
-    function setProfilePic () {
-        document.getElementById('profile_pic').innerHTML = `<img src="${bg.profilePic}" />`;
-    }
-}, false)
+		function setProfilePicFromMemory() {
+			chrome.storage.sync.get([ 'insta_profile_pic' ], function(result) {
+				document.getElementById('profile_pic').innerHTML = `<img src="${result.insta_profile_pic}" />`;
+			});
+		}
+
+		function setProfilePic() {
+			document.getElementById('profile_pic').innerHTML = `<img src="${bg.profilePic}" />`;
+		}
+	},
+	false
+);
